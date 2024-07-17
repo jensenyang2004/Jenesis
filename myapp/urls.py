016324ad4af1c615ccaca2310bc21c9b9c216a73
Jing-Cheng <jenseniu0810@gmail.com>
2024-07-17 17:44:50 +0800

    broken ui

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     #a url patterns
-    path("index/", views.index, name="index"), 
-    path('index/about/', views.about, name='about'),
+    path("", views.index, name="index"), 
+    path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('portfolio/', views.portfolio, name='portfolio'),
     path('service/', views.service, name = 'service')
