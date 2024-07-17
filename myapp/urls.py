ff3c760ef050eb7fbfc2e0eee458c7f6376b2823
dennis <lintinwei20030906@gmail.com>
2024-07-17 13:26:16 +0800

    add an html template

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,5 +4,10 @@ from . import views
 urlpatterns = [
     #a url patterns
     path("", views.home, name = "home"), 
-    path("todos/", views.todos, name="Todos")
+    path("todos/", views.todos, name="Todos"), 
+    path("index/", views.index, name="index"), 
+    path('guard/', views.guard, name='guard'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('service/', views.service, name='service')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -5,6 +5,21 @@ from .models import TodoItem
 def home(request):
     return render(request, "home.html")
 
+def index(request):
+    return render(request, "index.html")
+
+def guard(request):
+    return render(request, "guard.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def service(request):
+    return render(request, "service.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
 def todos(request):
     items = TodoItem.objects.all()
     return render(request, "todos.html", {"todos": items})
